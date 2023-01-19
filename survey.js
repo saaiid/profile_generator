e377c3113ed2e74b8076d73ad0d83c6e269d5e62
saaiid <ibrosakid@gmail.com>
2023-01-18 20:58:17 -0500

    Fun Profile Generator

---
+++ survey.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('What\'s your name? Nicknames are also acceptable :) ', (name) => {
+  rl.question('What\'s an activity you like doing? ', (activity) => {
+    rl.question('What do you listen to while doing that? ', (music) => {
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (meal) => {
+        rl.question('What\'s your favourite thing to eat for that meal? ', (food) => {
+          rl.question('Which sport is your absolute favourite? ', (sport) => {
+            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (superpower) => {
+              console.log(`${name} loves to ${activity} while listening to ${music}. ${name}'s favourite meal is ${meal} and favourite food is ${food}. ${name}'s favourite sport is ${sport} and ${name}'s superpower is ${superpower}.`);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
